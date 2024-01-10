8de4dcce9423c1b92b15bd103699135b356d0ba6
LorenzoEmanuele00 <lorenzoema2800@gmail.com>
2024-01-10 10:45:53 +0100

    add products file and class

---
+++ Models/products.php
@@ -0,0 +1,41 @@
+<?php
+
+include_once __DIR__ . '/category.php';
+
+class Product 
+{
+    protected $name;
+    protected $image;
+    protected $price;
+    public Category $category; // collega classe 
+
+    function __construct(Category $_category) {
+        $this->category = $_category;
+    }
+
+    public function setPrice($_price) 
+    {
+        if ($_price > 0)
+        {
+            $this->price = number_format($_price, 2);
+        }
+    }
+
+    public function getPrice() 
+    {
+       return $this->price;
+    }
+
+    public function setProdName($_name) 
+    {
+        if (strlen($_name) < 30)
+        {
+            $this->name = $_name;
+        }
+    }
+
+    public function getProdName() 
+    {
+       return $this->name;
+    }
+}
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 include_once __DIR__ . '/Models/category.php';
+include_once __DIR__ . '/Models/products.php';
 
 $cat = new Category();
 $cat->setName('Gatto');
@@ -10,5 +11,11 @@ $dog = new Category();
 $dog->setName('Cane');
 $dog->setIcon('dog.png');
 
+$cuccia = new Product($dog);
+$cuccia->setProdName('Cuccia Cane');
+$cuccia->setPrice(12.3);
+
+
 var_dump($cat);
 var_dump($dog);
+var_dump($cuccia);
